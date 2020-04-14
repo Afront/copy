5c58cb72921432cd49eb43dff6fd92fb0ca21cb4
Afront <3943720+Afront@users.noreply.github.com>
2020-04-14 23:57:54 +0900

    Add Copier class

--- src/copy.cr
+++ src/copy.cr
@@ -2,10 +2,24 @@
 module Copy
   VERSION = "0.1.0"
 
+  class Copier
+    def initialize
+    end
+
+    def copy(input_name : String, final_name : String)
+    end
+  end
+
+  copier = Copier.new
+
   case ARGV.size
   when 0
     p "An interactive shell has not been implemented yet. Please follow the following the format 'copy <filename> <new filename>' "
   when 1
+    # TODO: Check if input name is a directory?
+    copier.copy(ARGV.first, ARGV.first + " - copy")
   when 2
+    # TODO: Check if input name is a directory, and if output name is valid
+    copier.copy(ARGV.first, ARGV.last)
   end
 end
